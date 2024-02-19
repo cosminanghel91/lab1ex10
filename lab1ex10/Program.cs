326fa812cad218e1c3912d2bcd4294875c5e8248
Cosmin Anghel <cosmin.anghel@metaplast.ro>
2024-02-19 15:54:47 +0200

    Add project files.

---
+++ lab1ex10/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+* Scrieti un program care va afisa suma cifrelor unui numar intreg citit de la tastatura
+*/
+
+Console.WriteLine("Introduceti numarul pentru care doriti sa calculam suma cifrelor:");
+int numar = int.Parse(Console.ReadLine());
+
+int sum = 0, reminder;
+
+if
+    (numar < 100)
+{
+    while (numar > 0)
+    {
+        reminder = numar % 10;
+        sum = sum + reminder;
+        numar = numar / 10;
+
+    }
+    Console.WriteLine("Suma cifrelor este:" + sum);
+}
+else
+{
+    Console.WriteLine("Numarul are mai mult de 2 cifre");
+}
